3a2d0b40fb507607afbacfd3dad01cfab59d7ca4
Muhammad Talha Jahangir <59771511+jTECH1993@users.noreply.github.com>
2026-07-21 02:39:56 +0500

    Update server.ts

--- server.ts
+++ server.ts
@@ -2,7 +2,6 @@ import express from "express";
 import path from "path";
 import dotenv from "dotenv";
 import { createServer as createViteServer } from "vite";
-import { GoogleGenAI, Type } from "@google/genai";
 
 dotenv.config();
 
